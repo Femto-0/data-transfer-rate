b97aa4e9dd5768923dcb8d42dd3031a200fc6807
Ritesh Shah <arits.king74@gmail.com>
2024-01-21 11:05:31 -0600

    Application to calculate time taken to calculate data transfer rate via. different methods

---
+++ src/ApplicationClassForTransferRate.java
@@ -0,0 +1,32 @@
+public class ApplicationClassForTransferRate {
+    public static void main(String[] args) {
+        TransferRate tr= new TransferRate(150,5);
+//       tr.timeInDayHourMinSecFormat(tr.modemBestTransferRate(12000));
+//        tr.timeInDayHourMinSecFormat(tr.modemWorstTransferRate(3500));
+//        tr.timeInDayHourMinSecFormat(tr.v90(52));
+//        tr.timeInDayHourMinSecFormat(tr.t1Line(1.544));
+//       tr.timeInDayHourMinSecFormat(tr.fiberLine(622.06));
+//       tr.timeInDayHourMinSecFormat(tr.ethernet(1000));
+//       tr.timeInDayHourMinSecFormat(tr.eightOtwoDotElevenN(180));
+//     tr.timeInDayHourMinSecFormat(tr.threeG(2));
+         tr.timeInDayHourMinSecFormat(tr.fourG(20));
+//       tr.timeInDayHourMinSecFormat(tr.newHorizon(1000));
+
+
+          System.out.println("Total data: "+ tr.totalDataAfterOverhead()+ " bits");
+        System.out.println("fedEx:"+ tr.fedEx( 19)+ "bits/second");
+        System.out.println("Best modem transfer in "+tr.modemBestTransferRate(12000)+ " seconds");
+        System.out.println("Worst modem transfer in "+tr.modemWorstTransferRate(3500)+ " seconds");
+        System.out.println("v90 in: "+tr.v90(52)+ " seconds");
+        System.out.println("t1 line in: "+tr.t1Line(1.544)+ " seconds");
+        System.out.println("fiber line in: "+tr.fiberLine(622.06)+ " seconds");
+        System.out.println("Ethernet in: "+tr.ethernet(1000)+ " seconds");
+        System.out.println("8202.11N in: "+tr.eightOtwoDotElevenN(180)+ " seconds");
+        System.out.println("3G in: "+tr.threeG(2)+ " seconds");
+        System.out.println("4G in: "+tr.fourG(20)+ " seconds");
+        System.out.println("New horizon in: "+tr.newHorizon(1000)+ " seconds");
+
+
+
+    }
+}
---
+++ src/TransferRate.java
@@ -0,0 +1,132 @@
+public class TransferRate {
+
+    private double totalData;
+    private double overHeadValue;
+
+
+    TransferRate(double totalData, double overHeadValue) {
+        this.overHeadValue = overHeadValue;
+        this.totalData = totalData;
+    }
+
+    public double fromBytesToBits(double bytes) {
+        return bytes * 8;
+    }
+
+    public double fromKiloBytesToBytes(double kiloBytes) {
+        return kiloBytes * 1024;
+
+    }
+
+    public double fromMegaBytesToBytes(double megaBytes) {
+        return megaBytes * 1048576;
+    }
+
+    public double fromGigaBytesToBytes(double gigaBytes) {
+        return gigaBytes * 1073741824;
+    }
+
+    public double totalDataAfterOverhead() {  //this method is used to take total value of data to be transferred and adds the overhead value to it.
+        totalData = fromBytesToBits(fromGigaBytesToBytes(totalData));
+        return totalData + ((overHeadValue / 100) * totalData);
+
+
+    }
+
+    public double fedEx(double hours) {
+        return fromBytesToBits(fromGigaBytesToBytes(totalData)) / (hours * 60 * 60);
+    }
+
+    public double modemBestTransferRate(double bestTransferRate) { //transfer rate of modem is given in bps
+        return totalDataAfterOverhead() / bestTransferRate;
+    }
+
+    public double modemWorstTransferRate(double worstTransferRate) {  //transfer rate of modem is given in bps
+        return totalDataAfterOverhead() / worstTransferRate;
+    }
+
+    public double v90(double v90TransferRate) { //transfer rate of v90 is given in kbps
+        return totalDataAfterOverhead() / fromBytesToBits(fromKiloBytesToBytes(v90TransferRate));
+    }
+
+    public double t1Line(double t1LineTransferRate) { //transfer rate of t1 line is given in mbps
+        return totalDataAfterOverhead() / fromBytesToBits(fromMegaBytesToBytes(t1LineTransferRate));
+    }
+
+    public double fiberLine(double fiberLineRate) {//transfer rate of fiber line is given in mbps
+        return totalDataAfterOverhead() / fromBytesToBits(fromMegaBytesToBytes(fiberLineRate));
+    }
+
+    public double ethernet(double ethernetRate) {//transfer rate of ethernet is given in mbps
+        return totalDataAfterOverhead() / fromBytesToBits(fromMegaBytesToBytes(ethernetRate));
+    }
+
+    public double eightOtwoDotElevenN(double eightOtwoDotElevenNRate) {//transfer rate of eightOtwoDotElevenNRate is given in mbps
+        return totalDataAfterOverhead() / fromBytesToBits(fromMegaBytesToBytes(eightOtwoDotElevenNRate));
+    }
+
+    public double threeG(double threeGRate) {//transfer rate of threeGRate is given in mbps
+        return totalDataAfterOverhead() / fromBytesToBits(fromMegaBytesToBytes(threeGRate));
+    }
+
+    public double fourG(double fourGRate) {//transfer rate of fourGRate is given in mbps
+        return totalDataAfterOverhead() / fromBytesToBits(fromMegaBytesToBytes(fourGRate));
+    }
+
+    public double newHorizon(double newHorizonRate) { //transfer rate for new horizon probe is 1000bps and it takes 4 hours for the signals to reach to Earth
+        return (totalDataAfterOverhead() / newHorizonRate) + (4 * 60 * 60);
+    }
+
+    public void timeInDayHourMinSecFormat(double seconds) {
+        final int SECONDS_IN_DAY = 86400;
+        final int SECONDS_IN_HOUR = 3600;
+        final int SECONDS_IN_MINUTE = 60;
+
+        int days = 0;
+        int hours = 0;
+        int minutes = 0;
+        int second;
+
+        if (seconds >= SECONDS_IN_DAY) {
+            days = (int) seconds / SECONDS_IN_DAY;
+            int firstReminder = (int) seconds % SECONDS_IN_DAY;
+
+            if (firstReminder >= SECONDS_IN_HOUR) {
+                hours = firstReminder / SECONDS_IN_HOUR;
+                int secondReminder = firstReminder % SECONDS_IN_HOUR;
+
+                if (secondReminder >= SECONDS_IN_MINUTE) {
+                    minutes = secondReminder / SECONDS_IN_MINUTE;
+                    second = secondReminder % SECONDS_IN_MINUTE;
+                } else {
+                    second = secondReminder;
+                }
+            } else if (firstReminder >= SECONDS_IN_MINUTE) {
+                minutes = firstReminder / SECONDS_IN_MINUTE;
+                second = firstReminder % SECONDS_IN_MINUTE;
+            } else {
+                second = firstReminder;
+            }
+        } else if (seconds < SECONDS_IN_DAY && seconds >= SECONDS_IN_HOUR) {
+            hours = (int) seconds / SECONDS_IN_HOUR;
+            int secondReminder = (int) seconds % SECONDS_IN_HOUR;
+
+            if (secondReminder >= SECONDS_IN_MINUTE) {
+                minutes = secondReminder / SECONDS_IN_MINUTE;
+                second = secondReminder % SECONDS_IN_MINUTE;
+            } else {
+                second = secondReminder;
+            }
+        } else if (seconds < SECONDS_IN_HOUR && seconds >= SECONDS_IN_MINUTE) {
+            minutes = (int) seconds / SECONDS_IN_MINUTE;
+            second = (int) seconds % SECONDS_IN_MINUTE;
+        } else {
+            second = (int) seconds;
+        }
+
+        System.out.println(days + " days " + hours + " hours " + minutes + " minutes " + second + " seconds");
+    }
+
+
+
+}
